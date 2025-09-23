aaefa015df55a6ad2d04cdf02fe110070064dc9f
Ruheluobixieqing <2244216991@qq.com>
2025-09-23 16:02:22 +0800

    完善User类并学习Getter和Setter方法

--- src/main/java/com/example/backendpractice/entity/User.java
+++ src/main/java/com/example/backendpractice/entity/User.java
@@ -19,4 +19,44 @@ public class User {
 
     @Column(name = "timestamp", nullable = false)
     private LocalDateTime createdAt;
+
+    // 无参构造方法（必须有）
+    public User() {}
+
+    // 有参构造方法（ id 自动生成，故不包含）
+    public User(String username, String email, LocalDateTime createdAt){
+        this.username = username;
+        this.email = email;
+        this.createdAt = createdAt;
+    }
+
+    // Getter方法
+    public Long getId(){ 
+        return id; 
+    }
+    public String getUsername(){
+        return username;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public LocalDateTime getCreatedAt(){
+        return createdAt;
+    }
+
+    // Setter 方法
+    public void setUsername(String username){
+        this.username = username;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public void setCreatedAt(LocalDateTime createdAt){
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString(){
+        return "User{" + "id=" + id + ", username=" + username + ", email=" + email + ", createdAt=" + createdAt +"}";
+    }
 }
