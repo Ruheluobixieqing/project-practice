e20d0a93b8ba6edbcc9e9d3b0f7a0b07bff88311
Ruheluobixieqing <2244216991@qq.com>
2025-09-23 16:33:07 +0800

    更新setTimestamp方法的名称

--- src/main/java/com/example/backendpractice/entity/User.java
+++ src/main/java/com/example/backendpractice/entity/User.java
@@ -51,7 +51,7 @@ public class User {
     public void setEmail(String email){
         this.email = email;
     }
-    public void setCreatedAt(LocalDateTime createdAt){
+    public void setTimestamp(LocalDateTime createdAt){
         this.createdAt = createdAt;
     }
 
