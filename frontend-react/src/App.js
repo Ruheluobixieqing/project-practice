a745616d8a9be2a560f0e498ca862796a755d15b
= <=>
2025-09-29 14:31:42 +0800

    创建一个简单的用户管理页面功能介绍

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>用户管理系统 - React版本</h1>
+        <p>欢迎使用React重写的用户管理系统</p>
+
+        <div style={{ marginTop: '2rem'}}>
+          <h2>功能列表</h2>
+          <ul style={{ textAlign: 'left'}}>
+            <li>查看用户列表</li>
+            <li>添加新用户</li>
+            <li>编辑用户信息</li>
+            <li>删除用户</li>
+          </ul>
+        </div>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
